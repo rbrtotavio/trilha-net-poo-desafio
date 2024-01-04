b372848fdbbe044a3b66e9d7744d64449bef00e3
Roberto Leite <rbrtotavio@gmail.com>
2024-01-04 02:10:07 -0300

    Projeto concluído + Alteração do .csproj (.NET 8.0)

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// OK
+Console.WriteLine("Smartphone Nokia =============");
+Smartphone nokia = new Nokia(
+    numero:"24601", modelo: "Bookworm 12", imei: "40028922", memoria: 64);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Obsidian.md");
+
+Console.WriteLine("Smartphone Iphone ============");
+Smartphone iphone = new Iphone(
+    numero:"2345678", modelo: "IqDelicia 15", imei: "26122000", memoria: 128);
+
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Notion");
